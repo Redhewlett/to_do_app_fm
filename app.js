a3bb10c01590460759f05f14dd8a8bb35165a498
Teddy <edmondteddy@hotmail.fr>
2022-06-11 23:27:49 +0200

    displaying todos
    displaying todos, create their state, by default active : true

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ const todo_form = document.getElementById('sub_todo')
 const todos = document.getElementById('todos_list')
 const card_ctrl = document.querySelector('.card_ctrl')
 
-todo_form.addEventListener('submit', submit)
+todo_form.addEventListener('submit', submit_todo)
 
 //should we regex the input? of course, but later we should even prevent long strings
-function submit(e) {
+function submit_todo(e) {
   const new_todo = input.value
   e.preventDefault()
-  //   input.value ? console.log(input.value) : console.log('please enter something')
   print_todo(new_todo)
+  todo_form.reset()
 }
 
 function print_todo(task) {
   const randombId = Math.random().toString(36)
-  const new_todo_object = JSON.stringify({ id: randombId, task })
+  const new_todo_object = JSON.stringify({ id: randombId, task, active: true })
   if (task) {
     const new_todo_html = `<div class="todo_item">
         <div class="circle todo-circle"></div>
-        <p>${input.value}</p>
+        <p id=${randombId}>${input.value}</p>
         <img src="./images/icon-cross.svg" alt="x_mark" class="todo_item_x" />
+        <hr />
         </div>
-        <hr />`
+        `
     todos.innerHTML += new_todo_html
     save_todo(new_todo_object, randombId)
   } else {
@@ -32,7 +33,6 @@ function print_todo(task) {
 }
 
 function save_todo(task, id) {
-  const prefix = 'todos'
   const old_todos = localStorage.getItem('todos')
   if (!old_todos) {
     localStorage.setItem(`todos${id}`, task)
@@ -42,3 +42,28 @@ function save_todo(task, id) {
     console.log(all_todos)
   }
 }
+
+//load the todos
+function get_totos() {
+  const prefix = 'todos'
+
+  for (key in localStorage) {
+    if (key.startsWith(prefix)) {
+      const todo = JSON.parse(localStorage.getItem(key))
+      display_todos(todo)
+    }
+  }
+}
+
+function display_todos(tasks) {
+  todos.innerHTML += `<div class="todo_item">
+  <div class="circle todo-circle"></div>
+  ${tasks.active ? `<p id=${tasks.id}>${tasks.task}</p>` : `<p id=${tasks.id}><del>${tasks.task}</del></p>`}
+  <img src="./images/icon-cross.svg" alt="x_mark" class="todo_item_x" />
+  <hr />
+  </div>
+  `
+}
+
+// function calls
+get_totos()
